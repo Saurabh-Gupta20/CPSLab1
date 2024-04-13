0314145c824f89a11fe6802bfeea4c03a0bd5127
Saurabh-Gupta20 <saurabhgpta20@gmail.com>
2024-04-13 10:23:12 +0000

    Fibonacci series working with simple for loop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mbed.h"
+#include <cstdio>
 
-// a second commit
-// main() runs in its own thread in the OS
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 8;
+
 int main()
 {
+    //printf("Hello, lab program 1 \r\n");
+    printf("Fibonacci Sequence: %d, %d, ", t1, t2);
+    for(int i = 3; i <= max_terms; i++){
+        printf("%d, ", next_term);
+        t1 = t2;
+        t2 = next_term;
+        next_term = t1 + t2;
+    }
+    printf("\r\n");
     while (true) {
-
     }
 }
 
